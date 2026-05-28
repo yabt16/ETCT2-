fccc7330d5702e5e755161f7797d18185d8e11f6
yabt16 <c7f2f4dd9z@privaterelay.appleid.com>
2026-05-28 04:31:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,18 @@ document.getElementById("contactForm")
     e.preventDefault();
 
     alert("Message sent successfully!");
+
+});
+
+/* SIMPLE SCROLL EFFECT */
+
+window.addEventListener("scroll", function(){
+
+    const header = document.querySelector("header");
+
+    header.classList.toggle(
+        "sticky",
+        window.scrollY > 50
+    );
+
 });
